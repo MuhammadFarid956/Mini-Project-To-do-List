614154af784f74cbd5c4e98beea30b072e90d1d6
MuhammadFarid956 <mhmmdfaridirfn@gmail.com>
2025-07-10 06:35:48 +0700

    Update main.js

--- Mini-Project-Todo-List/JS/main.js
+++ Mini-Project-Todo-List/JS/main.js
@@ -1,21 +1,21 @@
-function addTask () {
-    document.getElementById("taskInput");
-    const dateInput = document.getElementById("dateInput");
+function addTask() {
+  document.getElementById("taskInput");
+  const dateInput = document.getElementById("dateInput");
 
-    const taskText = taskInput.value.trim();
-    const dateTxt = dateInput.value.trim();
+  const taskText = taskInput.value.trim();
+  const dateTxt = dateInput.value.trim();
 
-    // Validasi
-    if (taskText === "" || !/^\d{2}\/\d{2}\/\d{4}$/.test (datetext)) {
-        alert ("isi sing bener cok");
-        return;
-    } 
+  // Validasi
+  if (taskText === "" || !/^\d{2}\/\d{2}\/\d{4}$/.test(datetext)) {
+    alert("isi sing bener cok");
+    return;
+  }
 }
 
 // Filter Tasks
 function toggleFilter() {
-    const dropbtn = document.getElementById("dropdownContent");
- dropbtn.style.display = dropbtn.style.display === 'none' ? 'block' : 'none'
+  const dropbtn = document.getElementById("dropdownContent");
+  dropbtn.style.display = dropbtn.style.display === "none" ? "block" : "none";
 }
 
 // function filterTasks ( ) {
